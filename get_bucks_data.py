f7a2a278513e93d3054f23aafaab2dc3a1545470
Matthew Campbell <mjcampbell74@gmail.com>
2020-10-18 07:25:32 -0400

    added us and bucks download

---
+++ get_bucks_data.py
@@ -0,0 +1,28 @@
+# init
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+from IPython.display import display, HTML
+from pandas import DataFrame
+
+# set parameters
+days_back = 365
+
+# we can only get the past 7 days for bucks county with this API
+bucks_data = requests.get('https://localcoviddata.com/covid19/v1/cases/newYorkTimes?zipCode=19067&daysInPast=7')
+bucks_data = bucks_data.text
+bucks_data = pd.read_json(bucks_data)
+bucks_data = bucks_data.counties[0].get("historicData")
+bucks_data = DataFrame (bucks_data,columns=['date','deathCt','positiveCt'])
+bucks_data['date'] = pd.to_datetime(pd.Series(bucks_data['date']), format="%Y-%m-%d")
+
+# derive positive and death for bucks
+diff = bucks_data[['deathCt','positiveCt']].diff(periods=-1)
+bucks = bucks_data.join(diff, lsuffix='_caller', rsuffix='_other')
+bucks = bucks.rename(columns={'deathCt_caller':'death_total'
+                       ,'positiveCt_caller':'positive_total'
+                       ,'deathCt_other':'death'
+                       ,'positiveCt_other':'positive'})
+
+# Bucks death rate as of August 8, 2020
+bucks_case_rate = 7118/626370*100000
---
+++ get_us_data.py
@@ -0,0 +1,54 @@
+# init
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+from IPython.display import display, HTML
+from pandas import DataFrame
+
+# set parameters
+days_back = 365
+
+# get content from web APIs
+us = requests.get('https://api.covidtracking.com/v1/states/daily.json')
+us = us.text
+us = pd.read_json(us)
+
+# make function for the covid tracking api since they are all the same
+def clean_covidtracking_api_data(df):
+    
+    # get subset
+    df = df[['date','state','positiveIncrease','totalTestResultsIncrease'
+           ,'hospitalizedCurrently','death','deathIncrease','positive']]
+    
+    # Rename dataset columns
+    df = df.rename(columns={'positiveIncrease':'positive'
+                              ,'totalTestResultsIncrease': 'tests'
+                              ,'hospitalizedCurrently': 'hospitalized'
+                              ,'death': 'death_total'
+                              ,'deathIncrease': 'death'
+                              ,'positive': 'positive_total'})
+    
+    # Harmonize date column
+    df['date'] = df['date'].astype(str)
+    df['date'] = pd.to_datetime(pd.Series(df['date']), format="%Y%m%d")
+    
+    # Derived Columns
+    # Positivity Rate
+    positive = df.loc[:,['positive']].values[0:]
+    tests = df.loc[:,['tests']].values[0:]
+    df['pos_rate'] = positive / tests * 100
+
+    # Death Rate
+    deaths = df.loc[:,['death_total']].values[0:]
+    positive = df.loc[:,['positive_total']].values[0:]
+    df['death_rate'] = (deaths / positive) * 100
+    df['death_rate_est'] = (deaths / (positive * 10)) * 100
+    
+    return df
+
+# fix individual files
+us = clean_covidtracking_api_data(us)
+
+us.to_csv(r'daily_covid_us.csv')
+
+
