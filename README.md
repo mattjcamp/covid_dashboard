c6c9428d75a52ffe7c5f96a45e72a8ac190726df
Matthew Campbell <mjcampbell74@gmail.com>
2020-10-24 09:43:14 -0400

    added county data grab

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # covid_dashboard
 
 Uses available web API to combine PA, NJ, and Bucks covid data
+
+
+# Sources
+
+## County Population Totals: 2010-2019
+- US Census
+- https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html#par_textimage_70769902
--- process_all_county_data.py
+++ process_all_county_data.py
@@ -6,7 +6,12 @@ from IPython.display import display, HTML
 from pandas import DataFrame
 
 df = pd.read_csv(r'daily_covid_all_counties.csv')
+df = df.sort_values(by=['state','county','date'])
+diff = df[['cases','deaths']].diff(periods=1)
+df = df.join(diff, lsuffix='_today', rsuffix='_delta')
 
-df.to_csv(r'daily_covid_all_counties.csv')
+#df = df[(df['date'] > '2020-03-18')
 
-diff = df[['cases','deaths']].diff(periods=-1)
+df.head(25)
+
+# df.to_csv(r'covid_by_county.csv')
